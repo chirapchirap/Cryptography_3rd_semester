86c6c836ffc4abea59a010839e3ff6a28352a0f6
Vadim <vvadim.vvolkov.ps4@gmail.com>
2024-09-23 00:42:23 +0600

    ClientForm done

---
+++ Messenger_Lab1/Messenger/ClientForm/Client.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace ClientForm
+{
+    partial class Client
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(776, 337);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.Location = new System.Drawing.Point(12, 355);
+            this.textBoxMessage.Multiline = true;
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.Size = new System.Drawing.Size(776, 55);
+            this.textBoxMessage.TabIndex = 1;
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(12, 416);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(582, 96);
+            this.buttonSend.TabIndex = 2;
+            this.buttonSend.Text = "Отправить сообщение";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.Location = new System.Drawing.Point(600, 416);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(188, 96);
+            this.buttonRefresh.TabIndex = 3;
+            this.buttonRefresh.Text = "Переподключиться";
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
+            // 
+            // Client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 524);
+            this.Controls.Add(this.buttonRefresh);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBoxMessage);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Client";
+            this.Text = "Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBoxMessage;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.Button buttonRefresh;
+    }
+}
+
---
+++ Messenger_Lab1/Messenger/ClientForm/Client.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientForm
+{
+    public partial class Client : Form
+    {
+        private TcpClient client;
+        private NetworkStream stream;
+
+        public Client()
+        {
+            InitializeComponent();
+            TryConnectToServer();
+        }
+
+        // Метод для попытки подключения
+        private async void TryConnectToServer()
+        {
+            try
+            {
+                client = new TcpClient();
+                await client.ConnectAsync("127.0.0.1", 8080);  // Подключаемся к серверу
+                stream = client.GetStream();  // Получаем поток данных
+                richTextBox1.AppendText("Подключено к серверу.\n");
+                Task.Run(() => ReceiveMessages());  // Начинаем получать сообщения
+            }
+            catch (SocketException)  // Обрабатываем исключение при ошибке подключения
+            {
+                richTextBox1.Clear();
+                await Task.Delay(50);
+                richTextBox1.AppendText("Не удалось подключиться к серверу. Нажмите 'Обновить' для повторной попытки.\n");
+            }
+        }
+
+        // Метод для получения сообщений
+        private async Task ReceiveMessages()
+        {
+            byte[] buffer = new byte[1024];
+            int byteCount;
+            try
+            {
+                while ((byteCount = await stream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                {
+                    string message = Encoding.UTF8.GetString(buffer, 0, byteCount);
+                    richTextBox1.Invoke((Action)(() => richTextBox1.AppendText($"Сервер: {message}\n")));
+                }
+            }
+            catch (Exception)
+            {
+                // Оповещение о том, что клиент или сервер отключены
+                richTextBox1.Invoke((Action)(() =>
+                {
+                    richTextBox1.AppendText("Клиент отключен от сервера.\n");
+                }));
+            }
+        }
+
+        // Метод отправки сообщения
+        private async void buttonSend_Click(object sender, EventArgs e)
+        {
+            if (client != null && stream != null)
+            {
+                string message = textBoxMessage.Text;
+                byte[] data = Encoding.UTF8.GetBytes(message);
+
+                await stream.WriteAsync(data, 0, data.Length);
+                richTextBox1.AppendText($"Вы (клиент): {message}\n");
+                textBoxMessage.Clear();
+            }
+        }
+
+        // Кнопка для повторной попытки подключения
+        private void buttonRefresh_Click(object sender, EventArgs e)
+        {
+            if (client != null)
+            {
+                stream?.Close();
+                client?.Close();
+            }
+            richTextBox1.AppendText("Повторная попытка подключения...\n");
+            TryConnectToServer();  // Повторно подключаемся
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            stream?.Close();
+            client?.Close();
+        }
+    }
+}
