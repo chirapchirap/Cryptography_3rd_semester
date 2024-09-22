79f1b0424735f9bbe2c8c58b6707a557b270a226
Vadim <vvadim.vvolkov.ps4@gmail.com>
2024-09-23 00:41:11 +0600

    ServerForm done

---
+++ Messenger_Lab1/Messenger/ServerForm/Server.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace ServerForm
+{
+    partial class Server
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(776, 418);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.Location = new System.Drawing.Point(12, 436);
+            this.textBoxMessage.Multiline = true;
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.Size = new System.Drawing.Size(562, 77);
+            this.textBoxMessage.TabIndex = 1;
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(580, 436);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(205, 77);
+            this.buttonSend.TabIndex = 2;
+            this.buttonSend.Text = "Отправить сообщение";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 525);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBoxMessage);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBoxMessage;
+        private System.Windows.Forms.Button buttonSend;
+    }
+}
+
---
+++ Messenger_Lab1/Messenger/ServerForm/Server.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServerForm
+{
+    public partial class Server : Form
+    {
+        private TcpListener listener;
+        private TcpClient client;
+        private NetworkStream stream;
+
+        public Server()
+        {
+            InitializeComponent();
+            StartServer();
+        }
+
+        private async void StartServer()
+        {
+            listener = new TcpListener(IPAddress.Any, 8080);
+            listener.Start();
+            richTextBox1.AppendText("Сервер запущен... Ожидание соединений.\n");
+
+            client = await listener.AcceptTcpClientAsync();
+            stream = client.GetStream();
+            Task.Run(() => ReceiveMessages());
+
+            richTextBox1.AppendText("Клиент подключён.\n");
+        }
+
+        private async Task ReceiveMessages()
+        {
+            byte[] buffer = new byte[1024];
+            int byteCount;
+            try
+            {
+                while ((byteCount = await stream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                {
+                    string message = Encoding.UTF8.GetString(buffer, 0, byteCount);
+                    richTextBox1.Invoke((Action)(() => richTextBox1.AppendText($"Клиент: {message}\n")));
+                }
+            }
+            catch (Exception)
+            {
+                // Оповещение о том, что клиент или сервер отключены
+                richTextBox1.Invoke((Action)(() =>
+                {
+                    richTextBox1.AppendText("Клиент отключен от сервера.\n");
+                }));
+            }
+        }
+
+        private async void buttonSend_Click(object sender, EventArgs e)
+        {
+            if (client != null && stream != null)
+            {
+                string message = textBoxMessage.Text;
+                byte[] data = Encoding.UTF8.GetBytes(message);
+
+                await stream.WriteAsync(data, 0, data.Length);
+                richTextBox1.AppendText($"Вы (сервер): {message}\n");
+                textBoxMessage.Clear();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            stream?.Close();
+            client?.Close();
+            listener.Stop();
+        }
+    }
+}
