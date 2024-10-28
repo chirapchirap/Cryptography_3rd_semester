d080bb3f159c21b6150950107544e2197c1b6522
Vadim <vvadim.vvolkov.ps4@gmail.com>
2024-10-28 23:22:44 +0600

    Gui for MessengerServer

---
+++ RabinKeyExchangeMessenger_Lab3/MessengerApp/MessengerServer/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MessengerServer
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ RabinKeyExchangeMessenger_Lab3/MessengerApp/MessengerServer/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace MessengerServer
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void StartServerButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void StopServerButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
