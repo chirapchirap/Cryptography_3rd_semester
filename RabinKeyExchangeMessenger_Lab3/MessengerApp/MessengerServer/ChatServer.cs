e679bcb0ee7df4f8333fc1e66ef5215f05394dfa
Vadim <vvadim.vvolkov.ps4@gmail.com>
2024-10-28 18:35:11 +0600

    Add ChatServer class; initialized fields; add class ctor

---
+++ RabinKeyExchangeMessenger_Lab3/MessengerApp/MessengerServer/ChatServer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessengerServer
+{
+    internal class ChatServer
+    {
+        private TcpListener server;
+        private List<TcpClient> clients  = new List<TcpClient>();
+        private bool isRunning;
+
+        public ChatServer(string ip, int port)
+        {
+            server = new TcpListener(IPAddress.Parse(ip), port);
+        }
+    }
+}
