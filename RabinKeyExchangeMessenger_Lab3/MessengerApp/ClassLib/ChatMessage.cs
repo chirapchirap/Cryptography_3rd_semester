2aafcc6b186afe0daaed67ba251038defb7b1e33
Vadim <vvadim.vvolkov.ps4@gmail.com>
2024-11-01 03:32:01 +0600

    Added ChatMessage class for ClassLib

---
+++ RabinKeyExchangeMessenger_Lab3/MessengerApp/ClassLib/ChatMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLib
+{
+    public class ChatMessage
+    {
+        public required string Message { get; set; }
+        public required string SenderGuid { get; set; }
+        public required DateTime TimeStamp { get; set; }
+
+        public override string ToString()
+        {
+            return $"({TimeStamp:HH:mm:ss}) [{SenderGuid}]: {Message}";
+        }
+    }
+}
