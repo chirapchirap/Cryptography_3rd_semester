45b8ad3da26ba607800cd366f5fd16272c365277
Vadim <vvadim.vvolkov.ps4@gmail.com>
2024-11-04 15:13:14 +0600

    Added a method to check if a number is prime using the Rabin-Miller test

--- RabinKeyExchangeMessenger_Lab3/MessengerApp/ClassLib/RabinCryptoSystem.cs
+++ RabinKeyExchangeMessenger_Lab3/MessengerApp/ClassLib/RabinCryptoSystem.cs
@@ -13,7 +13,7 @@ namespace ClassLib
         private readonly BigInteger p;
         private readonly BigInteger q;
 
-        public BigInteger N {  get; }
+        public BigInteger N { get; }
 
         public RabinCryptoSystem()
         {
@@ -40,7 +40,62 @@ namespace ClassLib
             }
         }
 
+        private bool IsProbablePrime(BigInteger source, int certainty)
+        {
+            if (source == 2 || source == 3)
+            {
+                return true;
+            }
+            if (source < 2 || source % 2 == 0)
+            {
+                return false;
+            }
 
+            BigInteger d = source - 1;
+            int s = 0;
+            while (d % 2 == 0)
+            {
+                d /= 2;
+                s++;
+            }
 
+            using (var rng = System.Security.Cryptography.RandomNumberGenerator.Create())
+            {
+                byte[] bytes = new byte[source.ToByteArray().LongLength];
+                for (int i = 0; i < certainty; i++)
+                {
+                    BigInteger a;
+                    do
+                    {
+                        rng.GetBytes(bytes);
+                        a = new BigInteger(bytes);
+                    } while (a < 2 || a >= source - 2);
+
+                    BigInteger x = BigInteger.ModPow(a, d, source);
+                    if (x == 1 || x == source - 1)
+                    {
+                        continue;
+                    }
+
+                    for (int r = 1; r < s; r++)
+                    {
+                        x = BigInteger.ModPow(x, 2, source);
+                        if (x == 1)
+                        {
+                            return false;
+                        }
+                        if (x == source - 1)
+                        {
+                            break;
+                        }
+                    }
+                    if (x != source - 1)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
     }
 }
